00b22471263c80e831c6db12bc504f896781a693
Andy Ngo <andyngo2021@gmail.com>
2022-04-30 19:49:26 -0700

    wait what

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import './App.css';
 import Home from './components/Home'
+
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Route path="/" component={Home} />
+      </BrowserRouter>
     </div>
   );
 }
