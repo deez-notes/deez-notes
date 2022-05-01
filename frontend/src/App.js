6843adc38bcf936a683e695150a49b23369e4ba3
Andy Ngo <andyngo2021@gmail.com>
2022-04-30 20:20:20 -0700

    LETS GO TOMMY GOOD JOB

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './App.css';
 import Home from './components/Home'
-import { Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
 function App() {
