2037bf09807f263ddee4916385e5c4a66744ca16
Alexandros Konas <alexkonas@gmail.com>
2022-05-12 13:41:02 -0700

    added the option of calling CRUD functions with restaurant name

--- pythonBackend/testcode/testApp/app.py
+++ pythonBackend/testcode/testApp/app.py
@@ -11,8 +11,7 @@ hostname = "mongodb+srv://deeznotes:myrxsy7idiEvZCkE@cluster0.widrv.mongodb.net/
 
 app = FastAPI()
 client = motor.motor_asyncio.AsyncIOMotorClient(hostname)
-db = client.business
-
+restaurantsDB = client.business
 
 class PyObjectId(ObjectId):
     @classmethod
@@ -28,7 +27,6 @@ class PyObjectId(ObjectId):
     @classmethod
     def __modify_schema__(cls, field_schema):
         field_schema.update(type="string")
-
 
 class RestaurantModel(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
@@ -47,7 +45,6 @@ class RestaurantModel(BaseModel):
                 "course": "Fast Food"
             }
         }
-
 
 class UpdateRestaurantModel(BaseModel):
     name: Optional[str]
@@ -69,27 +66,36 @@ class UpdateRestaurantModel(BaseModel):
 @app.post("/", response_description="Add new restaurant", response_model=RestaurantModel)
 async def create_restaurant(restaurant: RestaurantModel = Body(...)):
     restaurant = jsonable_encoder(restaurant)
-    new_restaurant = await db.reviews.insert_one(restaurant)
-    created_restaurant = await db.reviews.find_one({"_id": new_restaurant.inserted_id})
+    new_restaurant = await restaurantsDB.reviews.insert_one(restaurant)
+    created_restaurant = await restaurantsDB.reviews.find_one({"_id": new_restaurant.inserted_id})
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_restaurant)
-
 
 @app.get(
     "/", response_description="List all restaurants", response_model=List[RestaurantModel]
 )
 async def list_restaurants():
-    restaurants = await db.reviews.find().to_list(1000)
+    restaurants = await restaurantsDB.reviews.find().to_list(1000)
     return restaurants
 
 
 @app.get(
-    "/{id}", response_description="Get a single restaurant", response_model=RestaurantModel
+    "/{id}", response_description="Get a single restaurant by ObjectID", response_model=RestaurantModel
 )
 async def show_restaurant(id: str):
-    if (restaurant := await db.reviews.find_one({"_id": ObjectId(id)})) is not None:
+    if (restaurant := await restaurantsDB.reviews.find_one({"_id": ObjectId(id)})) is not None:
         return restaurant
 
     raise HTTPException(status_code=404, detail=f"restaurant {ObjectId(id)} not found")
+
+
+@app.get(
+    "/reviews/{name}", response_description="Get a single restaurant by name", response_model=RestaurantModel
+)
+async def show_restaurant_by_name(name: str):
+    if (restaurant := await restaurantsDB.reviews.find_one({"name": name})) is not None:
+        return restaurant
+
+    raise HTTPException(status_code=404, detail=f"restaurant {name} not found")
 
 
 @app.put("/{id}", response_description="Update a restaurant", response_model=RestaurantModel)
@@ -97,25 +103,52 @@ async def update_restaurant(id: str, restaurant: UpdateRestaurantModel = Body(..
     restaurant = {k: v for k, v in restaurant.dict().items() if v is not None}
 
     if len(restaurant) >= 1:
-        update_result = await db.reviews.update_one({"_id": ObjectId(id)}, {"$set": restaurant})
+        update_result = await restaurantsDB.reviews.update_one({"_id": ObjectId(id)}, {"$set": restaurant})
 
         if update_result.modified_count == 1:
             if (
-                updated_restaurant := await db.reviews.find_one({"_id": ObjectId(id)})
+                updated_restaurant := await restaurantsDB.reviews.find_one({"_id": ObjectId(id)})
             ) is not None:
                 return updated_restaurant
 
-    if (existing_restaurant := await db.reviews.find_one({"_id": ObjectId(id)})) is not None:
+    if (existing_restaurant := await restaurantsDB.reviews.find_one({"_id": ObjectId(id)})) is not None:
         return existing_restaurant
 
     raise HTTPException(status_code=404, detail=f"Restaurant {ObjectId(id)} not found")
 
+@app.put("/reviews/{name}", response_description="Update a restaurant by name", response_model=RestaurantModel)
+async def update_restaurant_by_name(name: str, restaurant: UpdateRestaurantModel = Body(...)):
+    restaurant = {k: v for k, v in restaurant.dict().items() if v is not None}
+
+    if len(restaurant) >= 1:
+        update_result = await restaurantsDB.reviews.update_one({"name": name}, {"$set": restaurant})
+
+        if update_result.modified_count == 1:
+            if (
+                updated_restaurant := await restaurantsDB.reviews.find_one({"name": name})
+            ) is not None:
+                return updated_restaurant
+
+    if (existing_restaurant := await restaurantsDB.reviews.find_one({"name": name})) is not None:
+        return existing_restaurant
+
+    raise HTTPException(status_code=404, detail=f"Restaurant {name} not found")
+
 
 @app.delete("/{id}", response_description="Delete a restaurant")
 async def delete_restaurant(id: str):
-    delete_result = await db.reviews.delete_one({"_id": ObjectId(id)})
+    delete_result = await restaurantsDB.reviews.delete_one({"_id": ObjectId(id)})
 
     if delete_result.deleted_count == 1:
         return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
 
     raise HTTPException(status_code=404, detail=f"Restaurant {ObjectId(id)} not found")
+
+@app.delete("/reviews/{name}", response_description="Delete a restaurant by name")
+async def delete_restaurant_by_name(name: str):
+    delete_result = await restaurantsDB.reviews.delete_one({"name": name})
+
+    if delete_result.deleted_count == 1:
+        return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
+
+    raise HTTPException(status_code=404, detail=f"Restaurant {name} not found")
