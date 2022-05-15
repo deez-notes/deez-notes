847009199e900202e5bc4d4462a523e339a825d0
James Tseng <58379166+jtfa22@users.noreply.github.com>
2022-05-15 00:36:42 -0700

    Add Rating system

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -12,9 +12,13 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+import Rating from '@mui/material/Rating';
+
 
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import StarIcon from '@material-ui/icons/Star';
+
 
 import Spotify from 'react-spotify-embed';
 
@@ -48,19 +52,30 @@ return {
 };
 }
 
+// Helper Functions to generate Rating System
+// https://mui.com/material-ui/react-rating/
+const labels = {  null: '🔈', 1: '🔇',  2: '🔈',  3: '🔉',  4: '🔊',};
+
+function getLabelText(value) {
+  return `${value} Star${value !== 1 ? 's' : ''}, ${labels[value]}`;
+}
+
 // function will at some point need to have a Post class passed in containing data
 function Post () {
+  // rating
+  const [value, setValue] = React.useState(null);
+  const [hover, setHover] = React.useState(-1);
     return (
         <Card className={css.card} variant="outlined" elevation="24" sx={{borderRadius: 2 }}>
             <CardHeader 
             className={css.cardHeader}
-            avatar={<Avatar {...stringAvatar('Deez Notes')} />}
+            avatar={<Avatar {...stringAvatar('Rick Astley')} />}
             action={
                 <IconButton aria-label="settings">
                   <MoreVertIcon />
                 </IconButton>
               }
-            title="User Name"
+            title="Rick Astley"
             subheader="April 20, 2022"
             />
             <CardContent>
@@ -71,6 +86,25 @@ function Post () {
             <CardMedia>
               <Spotify wide link="https://open.spotify.com/track/4cOdK2wGLETKBW3PvgPWqT?si=be4a59344f8f4188" />
             </CardMedia>
+            <CardActions className={css.cardActions}>
+              <Rating
+                name="hover-feedback"
+                value={value}
+                max={4}
+                precision={1}
+                getLabelText={getLabelText}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+                onChangeActive={(event, newHover) => {
+                  setHover(newHover);
+                }}
+                emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+              />
+              {(
+                <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
+              )}
+            </CardActions>
         </Card>
     )
 
