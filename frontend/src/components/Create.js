660c0ddfd790290c4e8bfb9a9ff30b00ef61c1a7
Andy Ngo <andyngo2021@gmail.com>
2022-05-16 17:39:08 -0700

    basic create a post skeleton

--- frontend/src/components/Create.js
+++ frontend/src/components/Create.js
@@ -1,9 +1,22 @@
 // Create a post page
 import React from "react";
-
+import {TextField, Typography} from "@mui/material";
+import css from '../styles/Create.module.scss';
 function Create()
 {
-    return <div>yea</div>
+    return <div>
+        <br></br>
+        {/* still need to add dynamically changing text */}
+        <Typography>Create a Post</Typography>
+        
+        <div className={css.song}>
+            <TextField fullWidth id="song-title" label="Song Title" variant="outlined"/>
+            <TextField fullWidth id="song-title" label="Artist" variant="outlined"/>
+        </div>
+        <div className={css.descriptionBox}>
+            <TextField fullWidth multiline id="description" label="Post Description" variant="outlined"/>
+        </div>
+    </div>
 }
 
 export default Create;
