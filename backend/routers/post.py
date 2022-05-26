8aca6ac73b4d8153ca00c8cec4a94dfa7e1d7e08
Alexandros Konas <alexkonas@gmail.com>
2022-05-26 13:17:55 -0700

    updated post model to have comments be [username, comment] and have score (float) and like (int) fields

--- backend/routers/post.py
+++ backend/routers/post.py
@@ -4,7 +4,7 @@ from fastapi.responses import JSONResponse
 from bson import ObjectId
 from typing import List, Union
 # from pydantic import Field
-
+import pprint
 from models.general import idAndUsernameDependency
 from models.postModel import PostModel, UpdatePostModel
 from settings import client
@@ -20,12 +20,15 @@ class PostDependency():
         self.user = user
         self.tags = tags
         
-
 @router.post("/createpost", response_description="Create a new post", response_model=PostModel)
 async def create_post(post: PostModel = Body(...)):
+    if (not post.id):
+        post.id = str(ObjectId())
+   
     post = jsonable_encoder(post)
     new_post = await postDataDB.posts.insert_one(post)
     created_post = await postDataDB.posts.find_one({"_id": new_post.inserted_id})
+    # print(post)
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_post)
 
 @router.get("/", response_description="Get/list posts", response_model=Union[List[PostModel], PostModel])
@@ -34,7 +37,6 @@ async def get_post(commons: idAndUsernameDependency = Depends(), q: Union[List[s
     if(commons.objId):
         # print("searching by id")
         if (post := await postDataDB.posts.find_one({"_id": commons.objId})) is not None:
-            # print(post)
             return post
         raise HTTPException(status_code=404, detail=f"post {commons.objId} not found")
     elif(commons.user):
@@ -49,6 +51,7 @@ async def get_post(commons: idAndUsernameDependency = Depends(), q: Union[List[s
     else:
         # print("list all")
         posts = await postDataDB.posts.find().to_list(None)
+        # print(posts)
         return posts
 
 @router.put("/{id}", response_description="Update a post by id", response_model=PostModel)
@@ -64,7 +67,37 @@ async def update_post(id: str, post: UpdatePostModel = Body(...)):
             ) is not None:
                 return updated_post
 
-    if (existing_user := await postDataDB.posts.find_one({"_id": id})) is not None:
-        return existing_user
+    if (existing_post := await postDataDB.posts.find_one({"_id": id})) is not None:
+        return existing_post
+
+    raise HTTPException(status_code=404, detail=f"post {id} not found")
+
+@router.put("/comment/{id}", response_description="Comment on a post", response_model=PostModel)
+async def comment(id: str, current_user: str, comment: str):
+    
+    update_result = await postDataDB.posts.update_one({"_id": id}, {"$push": {"comments": [current_user, comment]}})
+    if update_result.modified_count == 1:
+        if (
+            updated_post := await postDataDB.posts.find_one({"_id": id})
+        ) is not None:
+            return updated_post
+
+    if (existing_post := await postDataDB.posts.find_one({"_id": id})) is not None:
+        return existing_post
+
+    raise HTTPException(status_code=404, detail=f"post {id} not found")
+
+@router.put("/rate/{id}", response_description="Rate a post", response_model=PostModel)
+async def comment(id: str, score: float):
+    
+    update_result = await postDataDB.posts.update_one({"_id": id}, {"$inc" : {"score": score, "likes": 1}})
+    if update_result.modified_count == 1:
+        if (
+            updated_post := await postDataDB.posts.find_one({"_id": id})
+        ) is not None:
+            return updated_post
+
+    if (existing_post := await postDataDB.posts.find_one({"_id": id})) is not None:
+        return existing_post
 
     raise HTTPException(status_code=404, detail=f"post {id} not found")
