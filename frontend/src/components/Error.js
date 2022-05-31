34198e232e377a658d9f6643646c638bcc2cc195
gpham2 <giangphamgia03@gmail.com>
2022-05-31 02:49:04 -0700

    made the error page super hot

--- frontend/src/components/Error.js
+++ frontend/src/components/Error.js
@@ -1,12 +1,31 @@
 import React from "react";
 import css from '../styles/Error.module.scss';
-
+import Image1 from '../STOPITGIANG.jpg' 
+import Box from '@mui/material/Box';
+import NavBar from "./NavBar"
 function Error()
 {
-    
+    document.body.style = 'background: white;';
     return (
         <>
+        <NavBar/>
+        
+            
+        <br/>
         <div id={css.title}>Uh Oh! This Page Doesn't Exist!</div>
+        <Box 
+        sx={{
+            
+            backgroundImage: `url(${Image1})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            marginBottom: '-170px'
+          }}
+        >
+            
     <div className={css.circles}>
       <p>404<br/>
        <small>PAGE NOT FOUND</small>
@@ -15,7 +34,11 @@ function Error()
       <span className={css.circleMed}></span>
       <span className={css.circleSmall}></span>
     </div>
+    </Box>
+    <div id={css.title}>Try a Different Page.</div>
+    <br/> <br/> <br/>
     </>
+    
     )
 }
 
